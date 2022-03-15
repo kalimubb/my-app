2a82996bdd09e020339bdafecb273939daa1464a
Kalimubb <mahjourkarim@gmail.com>
2022-03-15 12:22:55 +0100

    login files created and updated

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'React';
+import Card from 'Card';
+function App(){
+
+    return(
+        <>
+        <h1>Login page</h1>
+        <Card />
+        </>
+
+    )
+}
+
+export default App;
